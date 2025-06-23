d9d7bd09ed98091149d1381fe842aac130422684
hato8022 <tomo2969bimi@gmail.com>
2025-06-23 19:04:41 +0900

    class03

--- shader.js
+++ shader.js
@@ -2,8 +2,8 @@ class ManageShader {
     constructor(sdWidth, sdHeight) {
         this.validShaderDefinitions = [];
         this.loadedShaders = [];
-        this.currentActiveShaders = [];
         this.shaderLayers = [];
+        this.ActiveLayers = [];
         this.width = sdWidth;
         this.height = sdHeight;
     }
@@ -17,57 +17,71 @@ class ManageShader {
     }
 
     enableShader() {
-        if (this.validShaderDefinitions.length > 0) {
-            for (let i = 0; i < this.validShaderDefinitions.length; i++) {
-                this.loadedShaders[i] = { sd: loadShader('shader.vert', 'shader' + this.validShaderDefinitions[i] + '.frag'), count: 0, sdTime: 0 }
-                this.shaderLayers[i] = createGraphics(this.width, this.height, WEBGL);
+        if (this.validShaderDefinitions.length === 0) {
+            console.log("有効なshaderが存在していません。...");
+            return;
+        }
+
+        for (const defKey of this.validShaderDefinitions) {
+            // すでに読み込まれているかチェック
+            const existingShaderIndex = this.loadedShaders.findIndex(s => s.keyName === defKey);
+            if (existingShaderIndex === -1) { // まだ読み込まれていない場合のみ追加
+                const newShader = {
+                    keyName: defKey,
+                    sd: loadShader('shader.vert', 'shader' + defKey + '.frag'),
+                    count: 0,
+                    sdTime: 0
+                };
+                this.loadedShaders.push(newShader);
+                const newLayer = createGraphics(this.width, this.height, WEBGL);
+                newLayer.shader(newShader.sd);
+                this.shaderLayers.push(newLayer);
             }
         }
-        else {
-            console.log("有効なshaderが存在していません。defineVaildShader(shaderDefArray),addVaildShader(add)で追加してください")
-            return
+        // もしvalidShaderDefinitionsから削除されたものがloadedShadersに残るのが問題なら、別途クリーンアップロジックが必要
+    }
+
+    plusShaderCount(targetKey) {
+        const index = this.loadedShaders.findIndex(s => s.keyName === targetKey);
+        if (index !== -1) { // ★追加: indexが有効な場合にのみ処理を行う
+            this.loadedShaders[index].count += 1;
+            this.resetActiveLayers();
+        } else {
+            console.warn(`Warning: Shader with key '${targetKey}' not found.`);
         }
     }
 
-    plusShaderCount(searchKey) {
-        const index = this.loadedShaders.findIndex(s => s.keyNum === int(searchKey));
-        this.loadedShaders[index].count += 1;
-    }
-
-    validCurShader() {
+    plusSdTime() {
         for (let i = 0; i < this.loadedShaders.length; i++) {
             const BOOLVALUE = this.loadedShaders[i].count & 1;
-            if (BOOLVALUE == 1) {
-                this.currentActiveShaders.push(shader[i].sd);
+            if (BOOLVALUE === 1) {
+                this.loadedShaders[i].sdTime += 1;
             }
         }
     }
 
-    attachShader2Layer() {
-        for (let i = 0; i < this.currentActiveShaders.length; i++) {
-            this.shaderLayers[i] = shader(this.currentActiveShaders[i]);
-        }
-    }
-
-    SpliceShader() {
-        for (let i = this.loadedShaders.length - 1; i >= 0; i--) {
+    resetActiveLayers() {
+        this.ActiveLayers = [];
+        for (let i = 0; i < this.shaderLayers.length; i++) {
             const BOOLVALUE = this.loadedShaders[i].count & 1;
-            if (BOOLVALUE == 0) {
-                this.currentActiveShaders.splice(this.currentActiveShaders.indexOf(this.loadedShaders[i].sd), 1);
+            if (BOOLVALUE === 1) {
+                this.ActiveLayers.push(this.shaderLayers[i]);
+            }
+            else if(BOOLVALUE === 0){
+                this.loadedShaders.sdTime[i] = 0;
             }
         }
     }
 
-    checkShader() {
-        for (let i = 0; i < this.loadedShaders.length; i++) {
-            const BOOLVALUE = this.loadedShaders[i].count & 1;
-            if (BOOLVALUE == 1) {
-                this.currentActiveShaders.push(shader[i].sd);
-            }
-            else if (BOOLVALUE == 0) {
-                this.currentActiveShaders.splice(i, 1);
-            }
+    send2ShaderUniform(targetKey, uniformName, value) {
+        const index = this.loadedShaders.findIndex(s => s.keyName === targetKey);
+        if (index !== -1) { 
+            this.loadedShaders[index].sd.setUniform(uniformName, value);
+        } else {
+            console.warn(`Warning: Shader with key '${targetKey}' not found. Could not set uniform.`);
         }
     }
 
+    //オフにした時に経過時間をリセットする。
+    //オンの時
 }
